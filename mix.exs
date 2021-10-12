6b1ed42acd627206444fd513cc8f1f437ad5c568
Jakub Balinski <balinski.jakub@gmail.com>
2021-10-12 15:20:46 +0200

    bump package version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Membrane.Dashboard.MixProject do
   def project do
     [
       app: :membrane_dashboard,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
